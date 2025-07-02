239746d2fc9ece118eb166aea46cc200532a5e44
mihay <mihayablon@gmail.com>
2025-07-02 11:07:46 +0300

    Updating CORS

--- src/main/java/com/example/tradeservice/model/Trade.java
+++ src/main/java/com/example/tradeservice/model/Trade.java
@@ -3,10 +3,14 @@ package com.example.tradeservice.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Trade {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
